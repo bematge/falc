c069206bc94d7d34e704d341872da995529f2804
bematge <bertrand.matge@iledefrance.fr>
2015-05-13 01:40:21 +0200

    V0.5 RC1
    better CSS, added JS Menu

--- v0.4/js/dev.js
+++ v0.4/js/dev.js
@@ -1,53 +1,77 @@
+/* Fonciton de menu flottant */
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(function () {
+
+    var $sidebar = $("#sommaire"),
+        $window = $(window),
+        offset = $sidebar.offset(),
+        topPadding = 0;
+
+    $window.scroll(function () {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+});
+
 /* Fonciton de changement de classe pour le lien vers l'article affiché à l'écran*/
 
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
-	$(document).on("scroll", onScroll);
+    $(document).on("scroll", onScroll);
 
-	$('a[href^="#"]').on('click', function (e) {
-		e.preventDefault();
-		$(document).off("scroll");
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
 
-		$('a').each(function () {
-			$(this).removeClass('active');
-		})
-		$(this).addClass('active');
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
 
-		var target = this.hash;
-		$target = $(target);
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top + 2
-		}, 500, 'swing', function () {
-			window.location.hash = target;
-			$(document).on("scroll", onScroll);
-		});
-	});
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
 
 function onScroll(event) {
-		var scrollPosition = $(document).scrollTop();
-		$('nav a').each(function () {
-			var currentLink = $(this);
-			var refElement = $(currentLink.attr("href"));
-			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-				$('nav ul li a').removeClass("active");
-				currentLink.addClass("active");
-			} else {
-				currentLink.removeClass("active");
-			}
-		});
-	}
-	/* Fonction de changement de CSS à l'activation du lien de menu */
+        var scrollPosition = $(document).scrollTop();
+        $('nav a').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+                $('nav ul li a').removeClass("active");
+                currentLink.addClass("active");
+            } else {
+                currentLink.removeClass("active");
+            }
+        });
+    }
+    /* Fonction de changement de CSS à l'activation du lien de menu */
 
 
 function changeCSS(cssFile, cssLinkIndex) {
-	var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-	var newlink = document.createElement("link");
-	newlink.setAttribute("rel", "stylesheet");
-	newlink.setAttribute("type", "text/css");
-	newlink.setAttribute("href", cssFile);
-	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-	window.reload;
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    window.reload;
 }
